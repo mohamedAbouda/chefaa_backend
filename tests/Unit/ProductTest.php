feb3ee5b5f936d40be55bc48bf8e6fba66d2e85d
mohamed abouda <mohamed.aboda500@gmail.com>
2021-09-19 04:58:50 +0200

    finish product unit testing

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -2,17 +2,74 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Product;
+use App\Repositories\ProductRepository;
+use App\Services\ProductService;
+use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    protected $productService;
+    protected $faker;
+
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->faker = \Faker\Factory::create();
+        $this->productService = new ProductService(new ProductRepository());
+    }
+
+    /** @test */
+    public function it_can_create_a_product()
+    {
+        $data = [
+            'title'       => $this->faker->realText(20),
+            'description' => $this->faker->realText(200),
+            'image'       => $this->faker->image('public/storage/images', 400, 300, null, false),
+        ];
+
+        $product = $this->productService->create($data);
+        $this->assertInstanceOf(Product::class, $product);
+        $this->assertEquals($data['title'], $product->title);
+        $this->assertEquals($data['description'], $product->description);
+        $this->assertEquals($data['image'], $product->image);
+    }
+
+    /** @test */
+    public function it_can_update_a_product()
+    {
+        $data = [
+            // we can use factory to seed 1 product and use its ID here instead of selecting random one
+            'id'          => Product::inRandomOrder()->first()->id,
+            'title'       => $this->faker->realText(20),
+            'description' => $this->faker->realText(200),
+            'image'       => $this->faker->image('public/storage/images', 400, 300, null, false),
+        ];
+
+        $product = $this->productService->edit($data);
+
+        $this->assertEquals($product, true);
+    }
+
+    /** @test */
+    public function it_can_show_a_product()
+    {
+        $product = Product::factory()->create();
+        $found = $this->productService->getProduct($product->id);
+
+        $this->assertInstanceOf(Product::class, $found);
+        $this->assertEquals($found->title, $product->title);
+        $this->assertEquals($found->image, $product->image);
+        $this->assertEquals($found->description, $product->description);
+    }
+
+    /** @test */
+    public function it_can_destroy_a_product()
+    {
+        $product = Product::factory()->create();
+        $delete = $this->productService->delete($product->id);
+
+        $this->assertEquals($delete, true);
     }
 }
